87ace596a53e7900d621158b2abbb82e74a6ce7d
Prashant Chaubey <pvchoubey2000@gmail.com>
2025-09-05 21:43:31 +0530

    Restore app/page.tsx as homepage

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+﻿export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Digital Empire Blog</h1>
+      <p>A statically generated blog example using Next.js and Markdown.</p>
+    </main>
+  )
+}
