2f237143692f1d1d429a3cf5a792b11f4a93bf84
Prashant Chaubey <pvchoubey2000@gmail.com>
2025-09-06 03:38:19 +0530

    Next.js redirect / -> /blog

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      { source: "/", destination: "/blog", permanent: true },
+    ];
+  },
+};
+export default nextConfig;
