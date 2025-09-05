5162fa8dd9407763e1d7e2af4a6237efe5b2db0b
Prashant Chaubey <pvchoubey2000@gmail.com>
2025-09-05 20:35:28 +0530

    Add Google Analytics (G-ZDKCK2CT63)

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+﻿import '../styles/index.css';
+import Script from 'next/script';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZDKCK2CT63"
+        strategy="afterInteractive"
+      />
+      <Script id="ga-setup" strategy="afterInteractive">
+        {
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ZDKCK2CT63');
+        }
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
