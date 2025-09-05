15537593c98be4ddcabe44385262c63996b46d05
Prashant Chaubey <pvchoubey2000@gmail.com>
2025-09-05 21:13:30 +0530

    Fix GA embed in _document.js (no template string)

--- pages/_document.js
+++ pages/_document.js
@@ -6,14 +6,11 @@ export default function Document() {
       <Head>
         {/* Google Analytics */}
         <script async src='https://www.googletagmanager.com/gtag/js?id=G-ZDKCK2CT63'></script>
-        <script dangerouslySetInnerHTML={{
-          __html: \
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-ZDKCK2CT63');
-          \
-        }} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-ZDKCK2CT63');"
+          }}
+        />
       </Head>
       <body>
         <Main />
