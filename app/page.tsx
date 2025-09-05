6bfa4834eaa8f9338441e8fc0edcf3097e29ea48
Prashant Chaubey <pvchoubey2000@gmail.com>
2025-09-05 21:59:37 +0530

    Reset: fresh Next.js blog-starter (App Router)

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-﻿export default function Home() {
-  return (
-    <main>
-      <h1>Welcome to Digital Empire Blog</h1>
-      <p>A statically generated blog example using Next.js and Markdown.</p>
-    </main>
-  )
-}
