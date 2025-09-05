7168c429b63f253c8ee7a59584f13e4bda4ed271
Prashant Chaubey <pvchoubey2000@gmail.com>
2025-09-05 21:09:24 +0530

    Add GA via _document.js (fallback)

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+﻿import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang='en'>
+      <Head>
+        {/* Google Analytics */}
+        <script async src='https://www.googletagmanager.com/gtag/js?id=G-ZDKCK2CT63'></script>
+        <script dangerouslySetInnerHTML={{
+          __html: \
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZDKCK2CT63');
+          \
+        }} />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
